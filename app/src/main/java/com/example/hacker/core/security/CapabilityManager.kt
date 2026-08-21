8f6b7dc7cf3a046e4156094c17a8e964e00254dc
dm2123 <dm7178072@gmail.com>
2026-08-21 14:26:32 +0530

    Fix: use string literal for FEATURE_AUDIO_RECOGNITION

--- app/src/main/java/com/example/hacker/core/security/CapabilityManager.kt
+++ app/src/main/java/com/example/hacker/core/security/CapabilityManager.kt
@@ -22,5 +22,5 @@ object CapabilityManager {
     }
 
     fun supportsVoiceInteraction(context: Context): Boolean =
-        context.packageManager.hasSystemFeature(PackageManager.FEATURE_AUDIO_RECOGNITION)
+        context.packageManager.hasSystemFeature("android.software.audio_recognition")
 }
